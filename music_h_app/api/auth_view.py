27c1aba01e067eee47238d2aba1a2a389ed7025b
Yiyang Sun <2481898469@qq.com>
2025-06-04 18:02:21 +0800

    Update auth_view.py
    修改获取前端数据的方式POST为data

--- music_h_app/api/auth_view.py
+++ music_h_app/api/auth_view.py
@@ -42,8 +42,8 @@ class UserRegisterAPIView(APIView):
         
 class UserLoginAPIView(APIView):
     def post(self,request):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.data.get('username')
+        password = request.data.get('password')
         user = authenticate(request=request,username=username,password=password)
 
         if user is not None and user.is_active:
